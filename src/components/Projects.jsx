701c28375c39d547d1f67cdaacd5aedf84cb4a6c
mmh-masud03 <2054901003@student.bup.edu.bd>
2024-06-29 10:37:25 +0600

    project section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,8 +2,14 @@ import React from "react";
 
 function Projects() {
   return (
-    <div className="min-h-[80vh] text-white projects-dots-container">
-      Projects
+    <div className="min-h-[80vh] text-white project-dots-container z-50 ">
+      <p className="text-white font-bold text-3xl text-center w-full bg-slate-600/50 py-3">
+        Projects
+      </p>
+      <div className="flex flex-row justify-between text-white mt-3">
+        <div className="w-1/6">Software projects</div>
+        <div className="w-full bg-slate-100/50 h-[80vh]">Project cards</div>
+      </div>
     </div>
   );
 }
